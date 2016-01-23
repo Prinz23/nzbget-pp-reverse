4267556c1e45b37309a89a7660c94d58d322e998
Prinz23 <Prinz2311@gmail.com>
2016-01-24 00:41:00 +0100

    add support for subdirs

--- reverse_name.py
+++ reverse_name.py
@@ -16,9 +16,10 @@
 import os
 import sys
 import re
+import locale
 
-reverse_list = [r"\.\d{2}e\d{2}s\.", r"\.p0612\.", r"\.[pi]0801\.", r"\.p027\.", r"\.[pi]675\.", r"\.[pi]084\.", r"\.p063\.", r"\b[45]62[xh]\.", r"\.yarulb\.", r"\.vtd[hp]\.", 
-                r"\.ld[.-]?bew\.", r"\.pir.?(shv|dov|dvd|bew|db|rb)\.", r"\brdvd\.", r"\.vts\.", r"\.reneercs\.", r"\.dcv\.", r"\b(pir|mac)dh\b", r"\.reporp\.", r"\.kcaper\.", 
+reverse_list = [r"\.\d{2}e\d{2}s\.", r"\.p0612\.", r"\.[pi]0801\.", r"\.p027\.", r"\.[pi]675\.", r"\.[pi]084\.", r"\.p063\.", r"\b[45]62[xh]\.", r"\.yarulb\.", r"\.vtd[hp]\.",
+                r"\.ld[.-]?bew\.", r"\.pir.?(shv|dov|dvd|bew|db|rb)\.", r"\brdvd\.", r"\.vts\.", r"\.reneercs\.", r"\.dcv\.", r"\b(pir|mac)dh\b", r"\.reporp\.", r"\.kcaper\.",
                 r"\.lanretni\.", r"\b3ca\b", r"\bcaa\b", r"\b3pm\b", r"\.cstn\.", r"\.5r\.", r"\brcs\b"]
 reverse_pattern = re.compile('|'.join(reverse_list), flags=re.IGNORECASE)
 season_pattern = re.compile(r"(.*\.\d{2}e\d{2}s\.)(.*)", flags=re.IGNORECASE)
@@ -26,19 +27,117 @@ word_pattern = re.compile(r"([^A-Z0-9]*[A-Z0-9]+)")
 char_replace = [[r"(\w)1\.(\w)",r"\1i\2"]
 ]
 garbage_name = re.compile(r"^[a-zA-Z0-9]{3,}$")
-media_list = [r"\.s\d{2}e\d{2}\.", r"\.2160p\.", r"\.1080[pi]\.", r"\.720p\.", r"\.576[pi]\.", r"\.480[pi]\.", r"\.360p\.", r"\.[xh]26[45]\b", r"\.bluray\.", r"\.[hp]dtv\.", 
-              r"\.web[.-]?dl\.", r"\.(vhs|vod|dvd|web|bd|br).?rip\.", r"\.dvdr\b", r"\.stv\.", r"\.screener\.", r"\.vcd\.", r"\bhd(cam|rip)\b", r"\.proper\.", r"\.repack\.", 
+media_list = [r"\.s\d{2}e\d{2}\.", r"\.2160p\.", r"\.1080[pi]\.", r"\.720p\.", r"\.576[pi]\.", r"\.480[pi]\.", r"\.360p\.", r"\.[xh]26[45]\b", r"\.bluray\.", r"\.[hp]dtv\.",
+              r"\.web[.-]?dl\.", r"\.(vhs|vod|dvd|web|bd|br).?rip\.", r"\.dvdr\b", r"\.stv\.", r"\.screener\.", r"\.vcd\.", r"\bhd(cam|rip)\b", r"\.proper\.", r"\.repack\.",
               r"\.internal\.", r"\bac3\b", r"\baac\b", r"\bmp3\b", r"\.ntsc\.", r"\.pal\.", r"\.secam\.", r"\bdivx\b", r"\bxvid\b", r"\.r5\.", r"\.scr\."]
 media_pattern = re.compile('|'.join(media_list), flags=re.IGNORECASE)
 media_extentions = [".mkv", ".mp4", ".avi", ".wmv", ".divx", ".xvid"]
 
+if os.name == 'nt':
+    import ctypes
+
+    class win_eviron:
+        def getEnvironmentVariable(self, name):
+            name= unicode(name) # make sure string argument is unicode
+            n = ctypes.windll.kernel32.GetEnvironmentVariableW(name, None, 0)
+            if n == 0:
+                return None
+            buf = ctypes.create_unicode_buffer(u'\0'*n)
+            ctypes.windll.kernel32.GetEnvironmentVariableW(name, buf, n)
+            return buf.value
+
+        def __getitem__(self, key):
+            return self.getEnvironmentVariable(key)
+
+        def get(self, key, default=None):
+            r = self.getEnvironmentVariable(key)
+            return r if r else default
+
+    evn = win_eviron()
+else:
+    evn = os.environ
+
+SYS_ENCODING = None
+
+try:
+    locale.setlocale(locale.LC_ALL, '')
+except (locale.Error, IOError):
+    pass
+try:
+    SYS_ENCODING = locale.getpreferredencoding()
+except (locale.Error, IOError):
+    pass
+
+if not SYS_ENCODING or SYS_ENCODING in ('ANSI_X3.4-1968', 'US-ASCII', 'ASCII'):
+            SYS_ENCODING = 'UTF-8'
+
+
+class ek:
+    @staticmethod
+    def fixStupidEncodings(x, silent=False):
+        if type(x) == str:
+            try:
+                return x.decode(SYS_ENCODING)
+            except UnicodeDecodeError:
+                return None
+        elif type(x) == unicode:
+            return x
+        else:
+            return None
+
+    @staticmethod
+    def fixListEncodings(x):
+        if type(x) != list and type(x) != tuple:
+            return x
+        else:
+            return filter(lambda x: x != None, map(ek.fixStupidEncodings, x))
+
+    @staticmethod
+    def callPeopleStupid(x):
+        try:
+            return x.encode(SYS_ENCODING)
+        except UnicodeEncodeError:
+            return x.encode(SYS_ENCODING, 'ignore')
+
+    @staticmethod
+    def ek(func, *args, **kwargs):
+        if os.name == 'nt':
+            result = func(*args, **kwargs)
+        else:
+            result = func(*[ek.callPeopleStupid(x) if type(x) in (str, unicode) else x for x in args], **kwargs)
+
+        if type(result) in (list, tuple):
+            return ek.fixListEncodings(result)
+        elif type(result) == str:
+            return ek.fixStupidEncodings(result)
+        else:
+            return result
+
+
+class logger:
+    INFO = 'INFO'
+    DETAIL = 'DETAIL'
+    ERROR = 'ERROR'
+    WARNING = 'WARNING'
+
+    @staticmethod
+    def log(message, msg_type=INFO):
+        print('[%s] %s' % (msg_type, message))
+
+
+def tryInt(s, s_default=0):
+    try:
+        return int(s)
+    except:
+        return s_default
+
 # NZBGet V11+
 # Check if the script is called from nzbget 11.0 or later
-if os.environ.has_key('NZBOP_SCRIPTDIR') and not os.environ['NZBOP_VERSION'][0:5] < '11.0':
-    print "[INFO] Script triggered from NZBGet (11.0 or later)."
+nzbget_version = evn.get('NZBOP_VERSION', '0.1')
+nzbget_version = tryInt(nzbget_version[:nzbget_version.find(".")])
+if nzbget_version >= 11:
+    logger.log("Script triggered from NZBGet (11.0 or later).")
 
-    # NZBGet argv: all passed as environment variables.
-    clientAgent = "nzbget"
     # Exit codes used by NZBGet
     POSTPROCESS_PARCHECK=92
     POSTPROCESS_SUCCESS=93
@@ -48,48 +147,54 @@ if os.environ.has_key('NZBOP_SCRIPTDIR') and not os.environ['NZBOP_VERSION'][0:5
     # Check nzbget.conf options
     status = 0
 
-    if os.environ['NZBOP_UNPACK'] != 'yes':
-        print "[INFO] Please enable option \"Unpack\" in nzbget configuration file, exiting"
+    if evn['NZBOP_UNPACK'] != 'yes':
+        logger.log("Please enable option \"Unpack\" in nzbget configuration file, exiting")
         sys.exit(POSTPROCESS_NONE)
+
+    parstatus = evn['NZBPP_PARSTATUS']
 
     # Check par status
-    if os.environ['NZBPP_PARSTATUS'] == '3':
-        print "[INFO] Par-check successful, but Par-repair disabled, exiting"
+    if parstatus == '3':
+        logger.log("Par-check successful, but Par-repair disabled, exiting")
         sys.exit(POSTPROCESS_NONE)
 
-    if os.environ['NZBPP_PARSTATUS'] == '1':
-        print "[INFO] Par-check failed, setting status \"failed\""
+    if parstatus == '1':
+        logger.log("Par-check failed, setting status \"failed\"")
         status = 1
         sys.exit(POSTPROCESS_NONE)
+
+    unpackstatus = evn['NZBPP_UNPACKSTATUS']
 
     # Check unpack status
-    if os.environ['NZBPP_UNPACKSTATUS'] == '1':
-        print "[INFO] Unpack failed, setting status \"failed\""
+    if unpackstatus == '1':
+        logger.log("Unpack failed, setting status \"failed\"")
         status = 1
         sys.exit(POSTPROCESS_NONE)
 
-    if os.environ['NZBPP_UNPACKSTATUS'] == '0' and os.environ['NZBPP_PARSTATUS'] != '2':
+    directory = evn['NZBPP_DIRECTORY']
+
+    if unpackstatus == '0' and parstatus != '2':
         # Unpack is disabled or was skipped due to nzb-file properties or due to errors during par-check
 
-        for dirpath, dirnames, filenames in os.walk(os.environ['NZBPP_DIRECTORY']):
+        for dirpath, dirnames, filenames in ek.ek(os.walk, directory):
             for file in filenames:
-                fileExtension = os.path.splitext(file)[1]
+                fileExtension = ek.ek(os.path.splitext, file)[1]
 
                 if fileExtension in ['.par2']:
-                    print "[INFO] Post-Process: Unpack skipped and par-check skipped (although par2-files exist), setting status \"failed\"g"
+                    logger.log("Post-Process: Unpack skipped and par-check skipped (although par2-files exist), setting status \"failed\"g")
                     status = 1
                     break
 
-        if os.path.isfile(os.path.join(os.environ['NZBPP_DIRECTORY'], "_brokenlog.txt")) and not status == 1:
-            print "[INFO] Post-Process: _brokenlog.txt exists, download is probably damaged, exiting"
+        if ek.ek(os.path.isfile, ek.ek(os.path.join, directory, "_brokenlog.txt")) and not status == 1:
+            logger.log("Post-Process: _brokenlog.txt exists, download is probably damaged, exiting")
             status = 1
 
         if not status == 1:
-            print "[INFO] Neither par2-files found, _brokenlog.txt doesn't exist, considering download successful"
+            logger.log("Neither par2-files found, _brokenlog.txt doesn't exist, considering download successful")
 
     # Check if destination directory exists (important for reprocessing of history items)
-    if not os.path.isdir(os.environ['NZBPP_DIRECTORY']):
-        print "[INFO] Post-Process: Nothing to post-process: destination directory ", os.environ['NZBPP_DIRECTORY'], "doesn't exist"
+    if not ek.ek(os.path.isdir, directory):
+        logger.log("Post-Process: Nothing to post-process: destination directory %s doesn't exist" % directory)
         status = 1
 
     # All checks done, now launching the script.
@@ -98,12 +203,13 @@ if os.environ.has_key('NZBOP_SCRIPTDIR') and not os.environ['NZBOP_VERSION'][0:5
     videos = 0
     old_name = None
     new_name = None
-    for dirpath, dirnames, filenames in os.walk(os.environ['NZBPP_DIRECTORY']):
+    base_name = ek.ek(os.path.basename, directory)
+    for dirpath, dirnames, filenames in ek.ek(os.walk, directory):
         for file in filenames:
 
-            filePath = os.path.join(dirpath, file)
-            fileName, fileExtension = os.path.splitext(file)
-            dirname = os.path.dirname(filePath)
+            filePath = ek.ek(os.path.join, dirpath, file)
+            fileName, fileExtension = ek.ek(os.path.splitext, file)
+            dirname = ek.ek(os.path.dirname, filePath)
 
             if reverse_pattern.search(fileName) is not None:
                 na_parts = season_pattern.search(fileName)
@@ -119,27 +225,27 @@ if os.environ.has_key('NZBOP_SCRIPTDIR') and not os.environ['NZBOP_VERSION'][0:5
                     new_filename = new_words[::-1] + na_parts.group(1)[::-1]
                 else:
                     new_filename = fileName[::-1]
-                print "[INFO] reversing filename from: ", fileName, " to ", new_filename 
+                logger.log("reversing filename from: %s to %s" % (fileName, new_filename))
                 try:
-                    os.rename(filePath, os.path.join(dirpath, new_filename + fileExtension))
+                    ek.ek(os.rename, filePath, ek.ek(os.path.join, dirpath, new_filename + fileExtension))
                     rd = True
                 except Exception,e:
-                    print "[INFO] " + str(e)
-                    print "[INFO] Error: unable to rename file ", file
+                    logger.log(e, logger.ERROR)
+                    logger.log("Error: unable to rename file %s" % file, logger.ERROR)
                     pass
-            elif (fileExtension.lower() in media_extentions) and (garbage_name.search(fileName) is not None) and (media_pattern.search(os.path.basename(dirname)) is not None):
+            elif (fileExtension.lower() in media_extentions) and (garbage_name.search(fileName) is not None) and (media_pattern.search(base_name) is not None):
                 videos += 1
                 old_name = filePath
-                new_name = os.path.join(dirname, os.path.basename(dirname) + fileExtension)
+                new_name = ek.ek(os.path.join, dirname, '%s%s' % (base_name, fileExtension))
 
     if not rd and videos == 1 and old_name is not None and new_name is not None:
-        print "[INFO] renaming the File " + os.path.basename(old_name) + " to the Dirname " + os.path.basename(os.path.dirname(new_name))
+        logger.log("renaming the File %s  to the Dirname %s" % (ek.ek(os.path.basename, old_name), base_name))
         try:
-            os.rename(old_name, new_name)
+            ek.ek(os.rename, old_name, new_name)
             rd = True
         except Exception,e:
-            print "[INFO] " + str(e)
-            print "[INFO] Error unable to rename file ", old_name
+            logger.log(e, logger.ERROR)
+            logger.log("Error unable to rename file %s" % old_name, logger.ERROR)
             pass
 
     if rd:
@@ -148,5 +254,5 @@ if os.environ.has_key('NZBOP_SCRIPTDIR') and not os.environ['NZBOP_VERSION'][0:5
         sys.exit(POSTPROCESS_NONE)
 
 else:
-    print "[ERROR] This script can only be called from NZBGet (11.0 or later)."
+    logger.log("This script can only be called from NZBGet (11.0 or later).", logger.ERROR)
     sys.exit(0)
